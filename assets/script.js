6ec901af7c84358518357ef1a71b74a925b390ef
Andrea Gates <93196580+Andrea-Gates@users.noreply.github.com>
2023-01-30 12:50:14 +0000

    revised CSS and JS

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
-let NowMoment = moment().format("MMMM Do YYYY");
-let displayDate = document.getElementById("currentDay");
-let currentDate = moment().format("DDDD");
-let currentHour = moment().format("HH");
+// Get Current date and display it in the jumbotron
+let currentDay = $("#currentDay");
+currentDay.text (moment().format('dddd, MMM Do YYYY'));
 
 
-$(document).ready(function () {
-    //Moment.js code for current date and time
-    let NowMoment = moment().format("MMMM Do YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = NowMoment;
-    let currentHour = moment().format("HH");
-  
-    // Button function to clear local storage and clear contents
-    $("#clearFieldsBtn").click(function (event) {
-      event.preventDefault;
-      $("textarea").val("");
-      localStorage.clear();
+// Get sceduled item from local storage, if any
+$("#hour9 .description").val(localStorage.getItem("09"));
+$("#hour10 .description").val(localStorage.getItem("10"));
+$("#hour11 .description").val(localStorage.getItem("11"));
+$("#hour12 .description").val(localStorage.getItem("12"));
+$("#hour13 .description").val(localStorage.getItem("13"));
+$("#hour14 .description").val(localStorage.getItem("14"));
+$("#hour15 .description").val(localStorage.getItem("15"));
+$("#hour16 .description").val(localStorage.getItem("16"));
+$("#hour17 .description").val(localStorage.getItem("17"));
+
+timeTracker();
+
+
+    //  Button function to clear local storage and clear contents
+     $("#clearFieldsBtn").click(function (event) {
+       event.preventDefault;
+       $("textarea").val("");
+       localStorage.clear();
     });
